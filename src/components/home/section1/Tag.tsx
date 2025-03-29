829886f59479aa1d6814778dbe61b5d1cb271fc8
msun97 <godqhr9782@naver.com>
2025-03-29 23:28:57 +0900

    배포 오류 수정

--- src/components/home/section1/Tag.tsx
+++ src/components/home/section1/Tag.tsx
@@ -7,16 +7,20 @@ interface TagProps {
     className?: string; // className은 선택적 속성
     ref?: React.Ref<HTMLDivElement>; // ref는 선택적 속성
 }
-const Tag = React.forwardRef<HTMLDivElement, TagProps>(({ children, color, style, className }, ref) => {
-    return (
-        <div
-            ref={ref}
-            className={`p-1 w-fit text-[14px] absolute z-10 md:text-[19px] md:p-2 overflow-hidden whitespace-nowrap ${className}`}
-            style={{ backgroundColor: color, ...style }}
-        >
-            {children}
-        </div>
-    );
-};
 
+const Tag = React.forwardRef<HTMLDivElement, TagProps>(
+    ({ children, color, style, className }, ref) => {
+        return (
+            <div
+                ref={ref}
+                className={`p-1 w-fit text-[14px] absolute z-10 md:text-[19px] md:p-2 overflow-hidden whitespace-nowrap ${className}`}
+                style={{ backgroundColor: color, ...style }}
+            >
+                {children}
+            </div>
+        );
+    }
+);
+
+Tag.displayName = 'Tag';
 export default Tag;
