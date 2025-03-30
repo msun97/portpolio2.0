d3be370aacc07fc95cada903f65a38f01e2b09ae
msun97 <godqhr9782@naver.com>
2025-03-31 00:58:17 +0900

    배포 오류 수정

--- src/components/contact/Card.tsx
+++ src/components/contact/Card.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link';
 import React, { useMemo } from 'react';
 
-const Card = ({ modalName, handleOpenModal }) => {
+const Card = ({
+    modalName,
+    handleOpenModal,
+}: {
+    modalName: string | null;
+    handleOpenModal: (name: string) => void;
+}) => {
     const data = useMemo(() => {
         switch (modalName) {
             case 'recruit':
