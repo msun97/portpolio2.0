1b4e56e1b58cf50252a87ccf2a28f21943be2def
msun97 <godqhr9782@naver.com>
2025-03-31 00:56:42 +0900

    배포 오류 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -116,7 +116,7 @@ const Header = () => {
                     <div>
                         <Link href="/contact">
                             <p className="font-primaryRegular relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[1px] after:bg-black after:transition-all after:duration-300 hover:after:w-0 ">
-                                Let's Contact
+                                Let&apos;s Contact
                             </p>
                         </Link>
                     </div>
