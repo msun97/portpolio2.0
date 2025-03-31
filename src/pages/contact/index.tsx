37badf180aa9dd0500760bcdc2a3c63c72f83979
msun97 <godqhr9782@naver.com>
2025-03-31 18:16:33 +0900

    contact 반응형 추가

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -16,8 +16,8 @@ const Contact = () => {
     return (
         <section className=" w-full h-screen text-white overflow-hidden fixed z-[20]">
             <div className="brightness-50 bg-[url('/contactBg.jpg')] w-[110%] h-full absolute top-0 left-1/2 translate-x-[-50%] z-[-1] blur-[4px]" />
-            <div className="py-[80px]  h-[70%] flex flex-col justify-center items-center font-secondary ">
-                <div className="flex flex-col justify-center items-center text-[36px] mb-[100px]">
+            <div className="h-[40%] sm:py-[80px]  sm:h-[70%] flex flex-col justify-center items-center font-secondary ">
+                <div className="hidden sm:flex flex-col justify-center items-center text-[36px] mb-[100px]">
                     <p className="mb-10">FRONTEND DEVELOPER</p>
                     <div className="flex gap-[50px]">
                         <p>김미선</p>
@@ -25,14 +25,16 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="flex w-full justify-center gap-[10px]">
-                    <p className="font-primaryRegular text-[24px]">since</p>
-                    <p className="text-[128px]">CONTACT ME?</p>
-                    <p className="font-primaryRegular text-[24px]">2025</p>
+                    <p className="font-primaryRegular text-[24px] hidden sm:block">since</p>
+                    <p className="text-[clamp(40px,11vw,72px)] sm:text-[clamp(72px,9vw,100px)] md:text-[clamp(100px,6vw,128px)]">
+                        CONTACT ME?
+                    </p>
+                    <p className="font-primaryRegular text-[24px] hidden sm:block">2025</p>
                 </div>
             </div>
-            <div className="flex  w-full justify-center px-[55px] py-8 border-y border-y-[#9D9D9D] h-[30%] bg-[rgba(108,108,108,0.1)] backdrop-blur-lg text-[24px]">
+            <div className="flex flex-col sm:flex-row w-full justify-between sm:justify-center px-[55px] py-0 sm:py-8 border-y border-y-[#9D9D9D] h-[60%] sm:h-[30%] bg-[rgba(108,108,108,0.1)] backdrop-blur-lg text-[24px]">
                 <div
-                    className="flex flex-col justify-between w-1/3 border-r border-r-[#9D9D9D] px-5 cursor-pointer"
+                    className="flex items-center sm:items-start sm:flex-col justify-between h-1/3 sm:h-auto w-full sm:w-1/3 py-7 sm:py-0 border-b  border-b-[#9d9d9d] sm:border-b-0 sm:border-r sm:border-r-[#9D9D9D] px-5 cursor-pointer"
                     onClick={() => handleOpenModal('recruit')}
                 >
                     <div className="flex flex-col gap-2">
@@ -42,7 +44,7 @@ const Contact = () => {
                     <img src="/arrow-right.svg" className="w-[24px] h-[24px]" />
                 </div>
                 <div
-                    className="flex flex-col justify-between w-1/3 px-5  border-r border-r-[#9D9D9D] cursor-pointer"
+                    className="flex items-center sm:items-start sm:flex-col justify-between h-1/3 sm:h-auto w-full sm:w-1/3 py-7 sm:py-0 border-b  border-b-[#9d9d9d] sm:border-b-0 sm:border-r sm:border-r-[#9D9D9D] px-5 cursor-pointer"
                     onClick={() => handleOpenModal('email')}
                 >
                     <div className="flex flex-col gap-2 cursor-pointer">
@@ -52,7 +54,7 @@ const Contact = () => {
                     <img src="/arrow-right.svg" className="w-[24px] h-[24px]" />
                 </div>{' '}
                 <div
-                    className="flex flex-col justify-between w-1/3 px-5 cursor-pointer"
+                    className="flex items-center sm:items-start sm:flex-col justify-between h-1/3 sm:h-auto w-full sm:w-1/3 py-7 sm:py-0 border-b  border-b-[#9d9d9d] sm:border-b-0 px-5 cursor-pointer"
                     onClick={() => handleOpenModal('sns')}
                 >
                     <div className="flex flex-col gap-2">
